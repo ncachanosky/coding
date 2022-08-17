26d9b668f81115ad405d95f16ff662a486db42f5
Nicolas Cachanosky <44596340+ncachanosky@users.noreply.github.com>
2022-08-16 23:23:25 -0600

    Update test.py

--- python/matplotlib/test.py
+++ python/matplotlib/test.py
@@ -7,4 +7,3 @@ plt.style.use('https://github.com/ncachanosky/coding/blob/main/python/matplotlib
 # %% | PLOT EXAMPLE
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3]);  # Plot some data on the axes.
-# %%
