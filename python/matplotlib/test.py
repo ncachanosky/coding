ef9db53096e8f5ed7d0b416adf0c4f804983b36a
Nicolas Cachanosky <44596340+ncachanosky@users.noreply.github.com>
2022-08-16 23:11:26 -0600

    updates

--- python/matplotlib/test.py
+++ python/matplotlib/test.py
@@ -1,6 +1,10 @@
+# %% | IMPORT PACKAGES
 import matplotlib.pyplot as plt
 
+# %% | MATPLOTLIB STYLE AND FORMATS
 plt.style.use('https://github.com/ncachanosky/coding/blob/main/python/matplotlib/UTEP')
 
+# %% | PLOT EXAMPLE
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3]);  # Plot some data on the axes.
+# %%
