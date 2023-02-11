56ca61d930e385651ba8cc9eb5611a631b9f14f1
Nicolas Cachanosky <44596340+ncachanosky@users.noreply.github.com>
2023-02-11 16:25:33 -0700

    Update test.py

--- python/matplotlib/test.py
+++ python/matplotlib/test.py
@@ -6,10 +6,13 @@ from    matplotlib          import style
 
 # %% | MATPLOTLIB STYLE AND FORMATS
 # plt.style.use('my-paper')
-plt.style.use('https://github.com/ncachanosky/coding/blob/main/python/matplotlib/my-paper?raw=true')
+plt.style.use('https://github.com/ncachanosky/coding/blob/main/python/matplotlib/my-UTEP.mplstyle?raw=true')
+
 
 # %% | PLOT EXAMPLE
+
+plt.style.use('C:/Users/ncachanosky/OneDrive/GitHub/coding/python/matplotlib/my-UTEP.mplstyle')
+
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3]);  # Plot some data on the axes.
-
-# %%
+plt.title("My title", size="xx-large")
